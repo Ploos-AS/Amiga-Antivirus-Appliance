b50d3492c5fa5e9ca311c27c710950c9f84cf36d
pos <pgousdal@users.noreply.github.com>
2026-09-05 14:28:54 +0200

    Apply gofmt to IPF helper adapter

--- internal/preservation/ipf.go
+++ internal/preservation/ipf.go
@@ -13,11 +13,11 @@ import (
 )
 
 const (
-	IPFDecodeTimeout          = 15 * time.Second
-	MaxDerivedSectorImage     = 32 * 1024 * 1024
-	maxIPFMetadataLine        = 64 * 1024
-	maxIPFHelperStderr        = 64 * 1024
-	ipfHelperProtocolVersion  = 1
+	IPFDecodeTimeout         = 15 * time.Second
+	MaxDerivedSectorImage    = 32 * 1024 * 1024
+	maxIPFMetadataLine       = 64 * 1024
+	maxIPFHelperStderr       = 64 * 1024
+	ipfHelperProtocolVersion = 1
 )
 
 type DerivedSectorImage struct {
