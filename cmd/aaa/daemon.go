d8e69deb337366489a07e93df5c091e7563240fc
pos <pgousdal@users.noreply.github.com>
2026-09-07 10:25:53 +0200

    Apply gofmt alignment to M9.4 daemon

--- cmd/aaa/daemon.go
+++ cmd/aaa/daemon.go
@@ -72,8 +72,8 @@ func daemonCommand(args []string) {
 	defer cancel()
 
 	server := &http.Server{
-		Addr: *listen,
-		Handler: apihttp.NewHandlerWithSubmission(history, version, apihttp.SubmissionConfig{
+		Addr:              *listen,
+		Handler:           apihttp.NewHandlerWithSubmission(history, version, apihttp.SubmissionConfig{
 			Submitter:      manager,
 			IncomingRoot:  *incomingRoot,
 			MaxUploadBytes: *maxUploadBytes,
