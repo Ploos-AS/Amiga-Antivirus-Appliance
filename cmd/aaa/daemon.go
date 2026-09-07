bb76982595cbecaabee2c4551c961940a1e970db
pos <pgousdal@users.noreply.github.com>
2026-09-07 08:42:44 +0200

    Wire M9.1 scan history into daemon

--- cmd/aaa/daemon.go
+++ cmd/aaa/daemon.go
@@ -9,14 +9,18 @@ import (
 	"syscall"
 
 	"github.com/Ploos-AS/Amiga-Antivirus-Appliance/internal/daemon"
+	"github.com/Ploos-AS/Amiga-Antivirus-Appliance/internal/scanhistory"
 	"github.com/Ploos-AS/Amiga-Antivirus-Appliance/internal/scanner"
 )
+
+const defaultStateRoot = "/data/aaa/state"
 
 func daemonCommand(args []string) {
 	fs := flag.NewFlagSet("daemon", flag.ContinueOnError)
 	fs.SetOutput(os.Stderr)
 	workers := fs.Int("workers", 1, "number of concurrent scan workers")
 	queueDepth := fs.Int("queue-depth", 32, "maximum number of queued scans")
+	stateRoot := fs.String("state-root", stateRootFromEnv(), "persistent daemon state directory")
 	if err := fs.Parse(args); err != nil {
 		os.Exit(2)
 	}
@@ -25,7 +29,17 @@ func daemonCommand(args []string) {
 		os.Exit(2)
 	}
 
-	manager, err := daemon.New(*workers, *queueDepth, scanner.ScanFile)
+	history, err := scanhistory.New(*stateRoot)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "scan history failed: %v\n", err)
+		os.Exit(1)
+	}
+	if _, err := history.LoadLatest(); err != nil {
+		fmt.Fprintf(os.Stderr, "scan history replay failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	manager, err := daemon.NewWithRecorder(*workers, *queueDepth, scanner.ScanFile, history)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "daemon configuration failed: %v\n", err)
 		os.Exit(2)
@@ -34,7 +48,17 @@ func daemonCommand(args []string) {
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer stop()
 
-	fmt.Fprintf(os.Stderr, "AAA daemon started workers=%d queue-depth=%d\n", *workers, *queueDepth)
-	manager.Run(ctx)
+	fmt.Fprintf(os.Stderr, "AAA daemon started workers=%d queue-depth=%d history=%s\n", *workers, *queueDepth, history.Path())
+	if err := manager.Run(ctx); err != nil {
+		fmt.Fprintf(os.Stderr, "AAA daemon failed: %v\n", err)
+		os.Exit(1)
+	}
 	fmt.Fprintln(os.Stderr, "AAA daemon stopped")
+}
+
+func stateRootFromEnv() string {
+	if root := os.Getenv("AAA_STATE_ROOT"); root != "" {
+		return root
+	}
+	return defaultStateRoot
 }
